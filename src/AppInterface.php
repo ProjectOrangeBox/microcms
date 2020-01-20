307e67196d2092a4e9ce656d2778da94e64b935c
Don Myers <donmyers@me.com>
2020-01-20 13:54:10 -0500

    updates

--- src/AppInterface.php
+++ src/AppInterface.php
@@ -2,12 +2,10 @@
 
 namespace projectorangebox\cms;
 
-use projectorangebox\cms\AppInterface;
 use projectorangebox\cms\ContainerInterface;
 
 interface AppInterface
 {
-
 	public function __construct(ContainerInterface $container);
 	public function container(): ContainerInterface;
 	public function dispatch(): void;
--- support/public/index.php
+++ support/public/index.php
@@ -9,8 +9,12 @@ chdir(__ROOT__);
 /* Load composer auto loader */
 require 'vendor/autoload.php';
 
-$config = parse_ini_file(__ROOT__ . '/site/config.ini', true, INI_SCANNER_TYPED);
-$container = new \projectorangebox\cms\Container();
+error_reporting(E_ALL & ~E_NOTICE);
+ini_set('display_errors', 1);
+
+$config = array_replace(['services' => require __ROOT__ . '/vendor/projectorangebox/cms/src/Config/services.php'], parse_ini_file(__ROOT__ . '/site/config.ini', true, INI_SCANNER_TYPED));
 
 /* create container and application and dispatch */
-(new \projectorangebox\cms\service($config, $container))->dispatch();
+$app = new \projectorangebox\cms\App(new \projectorangebox\cms\Container($config));
+
+$app->dispatch();
